83349c43eac0ff1ef6d4045b013dbedc8ff029a1
MileneVieira <milenidionisio3@gmail.com>
2025-06-03 19:56:42 -0300

    css

---
+++ db/dados.js
@@ -0,0 +1,45 @@
+{
+    "clientes": [
+      {
+        "id": "1",
+        "cpf": "111.111.111-11",
+        "nome": "João Carlos Gouveia",
+        "telefone": "(18)9111-1111",
+        "cidade": "Presidente Prudente",
+        "uf": "SP",
+        "cep": "19015-010"
+      },
+      {
+        "id": "2",
+        "cpf": "222.222.222-22",
+        "nome": "Odete Roitman",
+        "telefone": "(18)9222-2222",
+        "cidade": "Londrina",
+        "uf": "PR",
+        "cep": "14023-013"
+      },
+      {
+        "id": "3",
+        "cpf": "333.333.333-33",
+        "nome": "Sandra Regina Vasconcelos",
+        "telefone": "(11)9333-3333",
+        "cidade": "São Paulo",
+        "uf": "SP",
+        "cep": "11033-029"
+      },
+      {
+        "id": "4",
+        "cpf": "444.444.444-44",
+        "nome": "Jason Grace",
+        "telefone": "(17)9444-4444",
+        "cidade": "Três Lagoas",
+        "uf": "MS",
+        "cep": "21035-070"
+      }
+    ],
+    "produtos": [],
+    "fornecedores": [],
+    "entregadores": [],
+    "categorias": [],
+    "usuarios": []
+  }
